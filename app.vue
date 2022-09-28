a48e864dd0803541e9f5df3a718cc35479a536b1
Igor <silent659@gmail.com>
2022-09-28 12:25:54 +0300

    added tag manager

--- app.vue
+++ app.vue
@@ -20,21 +20,34 @@ useHead({
 
 <template>
   <Head>
+    <!-- Google Tag Manager -->
+    <Script
+      >(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var
+      f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-M7KHCN9');</Script
+    >
+    <!-- End Google Tag Manager -->
     <Link rel="icon" type="image/png" href="/favicon.png"></Link>
     <Title>Разработка web сайтов</Title>
     <!-- Google tag (gtag.js) -->
     <Script async src="https://www.googletagmanager.com/gtag/js?id=G-LH2VSYHL92"></Script>
     <Script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag() {
-        dataLayer.push(arguments);
-      }
-      gtag("js", new Date());
-
+      window.dataLayer = window.dataLayer || []; function gtag() { dataLayer.push(arguments); } gtag("js", new Date());
       gtag("config", "G-LH2VSYHL92");
     </Script>
   </Head>
   <NuxtLayout>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript
+      ><iframe
+        src="https://www.googletagmanager.com/ns.html?id=GTM-M7KHCN9"
+        height="0"
+        width="0"
+        style="display: none; visibility: hidden"
+      ></iframe
+    ></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <NuxtPage />
     <Modal />
   </NuxtLayout>
