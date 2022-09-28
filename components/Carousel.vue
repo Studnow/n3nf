684a18e484da426dfb6ee7589339944fa15aab34
Igor <silent659@gmail.com>
2022-09-28 11:41:02 +0300

    changed path in carousel img

--- components/Carousel.vue
+++ components/Carousel.vue
@@ -22,7 +22,7 @@
       </div>
     </div>
     <div id="slide4" class="carousel-item relative w-full">
-      <img src="/img/first-full/slik-first.webp" class="w-full" />
+      <img src="/img/first-full/funiro-first.webp" class="w-full" />
       <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
         <a href="#slide3" class="btn btn-circle">❮</a>
         <a href="#slide1" class="btn btn-circle">❯</a>
