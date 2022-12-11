e910d3c33c6872d02a7e96f1c5603574027f33fd
Igor <silent659@gmail.com>
2022-12-11 02:12:47 +0200

    added gtag plugin, tag manager code commented

--- components/Carousel.vue
+++ components/Carousel.vue
@@ -1,31 +1,34 @@
 <template>
-  <div class="carousel carousel-center w-2/3 mx-auto">
-    <div id="slide1" class="carousel-item relative w-full">
-      <img src="/img/first-full/dbco-first.webp" class="w-full" />
-      <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-        <a href="#slide4" class="btn btn-circle">❮</a>
-        <a href="#slide2" class="btn btn-circle">❯</a>
+  <div class="container mx-auto">
+    <h3>Выполненные работы</h3>
+    <div class="carousel carousel-center w-2/3 mx-auto">
+      <div id="slide1" class="carousel-item relative w-full">
+        <img src="/img/first-full/dbco-first.webp" class="w-full" />
+        <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide4" class="btn btn-circle">❮</a>
+          <a href="#slide2" class="btn btn-circle">❯</a>
+        </div>
       </div>
-    </div>
-    <div id="slide2" class="carousel-item relative w-full">
-      <img src="/img/first-full/funiro-first.webp" class="w-full object-contain" />
-      <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-        <a href="#slide1" class="btn btn-circle">❮</a>
-        <a href="#slide3" class="btn btn-circle">❯</a>
+      <div id="slide2" class="carousel-item relative w-full">
+        <img src="/img/first-full/funiro-first.webp" class="w-full object-contain" />
+        <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide1" class="btn btn-circle">❮</a>
+          <a href="#slide3" class="btn btn-circle">❯</a>
+        </div>
       </div>
-    </div>
-    <div id="slide3" class="carousel-item relative w-full">
-      <img src="/img/first-full/houseVOP-first.webp" class="w-full object-contain" />
-      <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-        <a href="#slide2" class="btn btn-circle">❮</a>
-        <a href="#slide4" class="btn btn-circle">❯</a>
+      <div id="slide3" class="carousel-item relative w-full">
+        <img src="/img/first-full/houseVOP-first.webp" class="w-full object-contain" />
+        <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide2" class="btn btn-circle">❮</a>
+          <a href="#slide4" class="btn btn-circle">❯</a>
+        </div>
       </div>
-    </div>
-    <div id="slide4" class="carousel-item relative w-full">
-      <img src="/img/first-full/funiro-first.webp" class="w-full object-contain" />
-      <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-        <a href="#slide3" class="btn btn-circle">❮</a>
-        <a href="#slide1" class="btn btn-circle">❯</a>
+      <div id="slide4" class="carousel-item relative w-full">
+        <img src="/img/first-full/funiro-first.webp" class="w-full object-contain" />
+        <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide3" class="btn btn-circle">❮</a>
+          <a href="#slide1" class="btn btn-circle">❯</a>
+        </div>
       </div>
     </div>
   </div>
