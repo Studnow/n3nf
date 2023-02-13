6880785536d75b1af26b07a97d8fadc0797d8685
Igor <silent659@gmail.com>
2023-02-14 01:11:00 +0200

    target static

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  // target: 'static',
+  target: 'static',
   meta: {
     title: "Разработка web сайтов"
   },
