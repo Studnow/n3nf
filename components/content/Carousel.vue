370bcf2735be821a2ab406e2bf609fd0dc81b780
Igor <silent659@gmail.com>
2023-02-26 17:51:32 +0200

    commented carousel iframe slide

--- components/content/Carousel.vue
+++ components/content/Carousel.vue
@@ -2,14 +2,14 @@
   <div class="container mx-auto mt-20">
     <h3 class="text-center text-2xl font-medium">Примеры работ</h3>
     <div class="carousel carousel-center mx-auto mt-10">
-      <div id="slide1" class="carousel-item relative w-full">
-        <!-- <img src="/img/first-full/dbco-first.webp" class="w-full" /> -->
+      <!-- <div id="slide1" class="carousel-item relative w-full">
+        <img src="/img/first-full/dbco-first.webp" class="w-full" />
         <PortfolioFrames />
         <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <a href="#slide4" class="btn btn-circle">❮</a>
           <a href="#slide2" class="btn btn-circle">❯</a>
         </div>
-      </div>
+      </div> -->
       <div id="slide2" class="carousel-item relative w-full">
         <img src="/img/first-full/funiro-first.webp" class="w-full object-contain" />
         <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
