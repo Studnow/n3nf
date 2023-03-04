d158b1ca521b3839d833dd17c5d8a13cd96bbda5
Igor <silent659@gmail.com>
2023-03-04 21:10:34 +0200

    added loading lazy to carousel

--- components/content/Carousel.vue
+++ components/content/Carousel.vue
@@ -11,21 +11,21 @@
         </div>
       </div> -->
       <div id="slide2" class="carousel-item relative w-full">
-        <img src="/img/first-full/funiro-first.webp" class="w-full object-contain" />
+        <img loading="lazy" src="/img/first-full/funiro-first.webp" class="w-full object-contain" />
         <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <a href="#slide1" class="btn btn-circle">❮</a>
           <a href="#slide3" class="btn btn-circle">❯</a>
         </div>
       </div>
       <div id="slide3" class="carousel-item relative w-full">
-        <img src="/img/first-full/houseVOP-first.webp" class="w-full object-contain" />
+        <img loading="lazy" src="/img/first-full/houseVOP-first.webp" class="w-full object-contain" />
         <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <a href="#slide2" class="btn btn-circle">❮</a>
           <a href="#slide4" class="btn btn-circle">❯</a>
         </div>
       </div>
       <div id="slide4" class="carousel-item relative w-full">
-        <img src="/img/first-full/funiro-first.webp" class="w-full object-contain" />
+        <img loading="lazy" src="/img/first-full/funiro-first.webp" class="w-full object-contain" />
         <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <a href="#slide3" class="btn btn-circle">❮</a>
           <a href="#slide1" class="btn btn-circle">❯</a>
