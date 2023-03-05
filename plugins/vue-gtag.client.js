b3264b1602669ddd65d3be83776e0dc9c0a3336c
Igor <silent659@gmail.com>
2023-03-05 04:59:43 +0200

    add gtm-support/vue-gtm

--- plugins/vue-gtag.client.js
+++ plugins/vue-gtag.client.js
@@ -1,9 +1,15 @@
 import VueGtag from "vue-gtag-next";
+import { createGtm } from "@gtm-support/vue-gtm";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(VueGtag, {
-    property: {
-      id: "G-LH2VSYHL92",
-    },
-  });
+  // nuxtApp.vueApp.use(VueGtag, {
+  //   property: {
+  //     id: "G-LH2VSYHL92",
+  //   },
+  // });
+  nuxtApp.vueApp.use(
+    createGtm({
+      id: "GTM-M7KHCN9",
+    })
+  );
 });
